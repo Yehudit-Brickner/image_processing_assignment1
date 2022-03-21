42f8d90fc3554c152aea7bbbcba9354f404ec467
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-21 13:50:34 +0200

    started to work on the funcs

--- ex1_utils.py
+++ ex1_utils.py
@@ -11,6 +11,9 @@
 from typing import List
 
 import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
 LOAD_GRAY_SCALE = 1
 LOAD_RGB = 2
 
@@ -20,8 +23,12 @@ def myID() -> np.int:
     Return my ID (not the friend's ID I copied from)
     :return: int
     """
-    return 123456789
+    return 328601018
 
+
+
+def NormalizeData(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
 
 def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     """
@@ -30,7 +37,17 @@ def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     :param representation: GRAY_SCALE or RGB
     :return: The image object
     """
-    pass
+    print("reading img")
+    if representation == 2:
+        img_color = cv2.imread(filename)
+        img_color = cv2.cvtColor(img_color, cv2.COLOR_BGR2RGB)
+        img_color = NormalizeData(img_color)
+        return img_color
+    else:
+        img_gray = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        img_gray = NormalizeData(img_gray)
+        return img_gray
+    # pass
 
 
 def imDisplay(filename: str, representation: int):
@@ -40,7 +57,15 @@ def imDisplay(filename: str, representation: int):
     :param representation: GRAY_SCALE or RGB
     :return: None
     """
-    pass
+    img = imReadAndConvert(filename, representation)
+    if representation == 1:
+        # By default, matplotlib use a colormap which maps intensities to colors. so without the cmap='gray' the bic whold be blueish yellow
+        plt.imshow(img, cmap='gray')
+        plt.show()
+    else:
+        plt.imshow(img)
+        plt.show()
+    # pass
 
 
 def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
@@ -49,7 +74,27 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
     :param imgRGB: An Image in RGB
     :return: A YIQ in image color space
     """
-    pass
+    r = imgRGB[:, :, 0]
+    g = imgRGB[:, :, 1]
+    b = imgRGB[:, :, 2]
+    # y = r * 0.299 + g * 0.587 + b * 0.114
+    # i = r * 0.595879 + g * -0.274133 + b * -0.321746
+    # q = r * 0.211205 + g * -0.523083 + b * 0.311878
+    y = r*0.299 +g*0.587 +b*0.114
+    i = r*0.5959 +g*-0.2746 +b*-0.3213
+    q = r*0.2115 +g*-0.5227 +b*0.3112
+    yiq_img = imgRGB
+    yiq_img[:, :, 0] = y
+    yiq_img[:, :, 1] = i
+    yiq_img[:, :, 2] = q
+    # print("image reg")
+    # print(image)
+    # print("image yiq")
+    # print(yiq_img)
+    #     yiq_img=NormalizeData(yiq_img)
+    plt.imshow(yiq_img)
+    plt.show()
+    # pass
 
 
 def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
@@ -58,7 +103,24 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     :param imgYIQ: An Image in YIQ
     :return: A RGB in image color space
     """
-    pass
+    y = imgYIQ[:, :, 0]
+    i = imgYIQ[:, :, 1]
+    q = imgYIQ[:, :, 2]
+    r = y + i * 0.956 + q * 0.619
+    g = y + i * -0.272 + q * -0.647
+    b = y + i * -1.106 + q * 1.703
+    rgb_img = imgYIQ
+    rgb_img[:, :, 0] = r
+    rgb_img[:, :, 1] = g
+    rgb_img[:, :, 2] = b
+    # print("image reg")
+    # print(image)
+    # print("image yiq")
+    # print(yiq_img)
+    #     yiq_img=NormalizeData(yiq_img)
+    plt.imshow(rgb_img)
+    plt.show()
+    # pass
 
 
 def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
