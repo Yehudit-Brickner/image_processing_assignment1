a2cbb51a5dc0a7022ad13db5e28e9ffd3d42c70b
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-28 10:11:13 +0300

    surounded by try and catch

--- gamma.py
+++ gamma.py
@@ -39,28 +39,28 @@ def gammaDisplay(img_path: str, rep: int):
     else:
         img_color = cv2.imread(img_path)
         img_color = NormalizeData((img_color))
+    # i am using try and exempt to catch errors that will stop the main from running
+    try:
+        # create a separate window named 'image' for trackbar
+        cv2.namedWindow('image')
+        # create trackbar in 'image' window with name 'gamma''
+        cv2.createTrackbar('gamma', 'image', 0, 200,nothing)
 
+        while (1):
 
-    # create a separate window named 'image' for trackbar
-    cv2.namedWindow('image')
-    # create trackbar in 'image' window with name 'gamma''
-    cv2.createTrackbar('gamma', 'image', 0, 200,nothing)
+            gamma= cv2.getTrackbarPos('gamma', 'image')/100.0
+            # the gamma value will br the value of thr traker divided by 100.0 so that tha gamma value will actuallly be netwen 0 and 1
 
-
-
-    while (1):
-        gamma= cv2.getTrackbarPos('gamma', 'image')/100.0
-        # the gamma value will br the value of thr traker divided by 100.0 so that tha gamma value will actuallly be netwen 0 and 1
-
-        if rep == 1:
-            img_gray_copy = img_gray ** gamma # create a copy of the image array raised to the power of the gamma
-            cv2.imshow('image', img_gray_copy)
-        else:
-            img_color_copy = img_color ** gamma # create a copy of the image array raised to the power of the gamma
-            cv2.imshow('image',img_color_copy)
-        cv2.waitKey(1) # wait 1 milli sec before going back to the beginning of the while
-    cv2.destroyAllWindows() # destroys the window
-
+            if rep == 1:
+                img_gray_copy = img_gray ** gamma # create a copy of the image array raised to the power of the gamma
+                cv2.imshow('image', img_gray_copy)
+            else:
+                img_color_copy = img_color ** gamma # create a copy of the image array raised to the power of the gamma
+                cv2.imshow('image',img_color_copy)
+            cv2.waitKey(1) # wait 1 milli sec before going back to the beginning of the while
+        cv2.destroyAllWindows() # destroys the window
+    except:
+        print("an error was caught")
 
 
 
@@ -69,7 +69,7 @@ def gammaDisplay(img_path: str, rep: int):
 
 def main():
    gammaDisplay('bac_con.png', LOAD_GRAY_SCALE)
-
+   gammaDisplay('sunset1.jpg', LOAD_GRAY_SCALE)
 
 if __name__ == '__main__':
     main()
